9a009d459ea04be1e674fdf1e09c75f08af66869
Pavithra0201 <pavi020105@gmail.com>
2025-06-30 00:47:45 +0530

    Time: 4 ms (18.2%), Space: 97.3 MB (86.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       int maxProfit=0;
-       int minPrice=INT_MAX;
+        int minPrice=INT_MAX;
+        int maxProfit=0;
+
         for (int price:prices)
         {
             minPrice=min(minPrice,price);
             maxProfit=max(maxProfit,price-minPrice);
         }
-       
-       return maxProfit;
+        return maxProfit;
+        
     }
 };
