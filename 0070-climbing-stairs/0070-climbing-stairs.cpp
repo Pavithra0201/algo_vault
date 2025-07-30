288afbd0e5c97229b2ee63e8518396ae251ceac0
Pavithra0201 <pavi020105@gmail.com>
2025-07-30 23:04:33 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.16%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n<=1)
+            return 1;
+        int a=1,b=1,c;
+        for (int i=2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+
+        return b;
+
+      
+        
+        
+        
+    }
+};
